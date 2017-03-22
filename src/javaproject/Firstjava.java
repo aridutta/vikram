632653e7e78fc886c3181af812a0862261c0a033
aridutta <arindam.dutta@wildnettechnologies.com>
2017-03-22 16:11:55 +0530

    update

--- src/javaproject/Firstjava.java
+++ src/javaproject/Firstjava.java
@@ -5,12 +5,18 @@ public class Firstjava {
 	public void show() {
 		System.out.println("hello i am show method");
 	}
+	public void masterDemo() {
+
+		System.out.println("This is a master demo");
+}
 
 	public void  display(){
 		System.out.println("hello i am show method");
 	}
 	public static void main(String[] args) {
+		
 		Firstjava obj=new Firstjava();
+		obj.masterDemo();
 		Data obj2=new Data();
 		obj.show();
 		obj.display();
