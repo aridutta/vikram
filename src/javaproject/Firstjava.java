6c6580ab82f9ed7311a244301bf03dc8104b5f56
aridutta <arindam.dutta@wildnettechnologies.com>
2017-03-22 15:35:11 +0530

    This is a master test

--- src/javaproject/Firstjava.java
+++ src/javaproject/Firstjava.java
@@ -10,12 +10,19 @@ public class Firstjava {
 		System.out.println("hello i am show method");
 	}
 
+	public void masterDemo() {
+
+		System.out.println("This is a master demo");
+	}
+
 	public static void main(String[] args) {
 
 		Firstjava obj = new Firstjava();
-		Data obj2 = new Data();
+		obj.masterDemo();
 		obj.show();
 		obj.display();
+		Data obj2 = new Data();
+
 		obj2.sum(2, 3);
 	}
 }
