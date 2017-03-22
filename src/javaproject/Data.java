36a069fa83028f332f9d742fc2e99a6de81083f7
aridutta <arindam.dutta@wildnettechnologies.com>
2017-03-22 15:50:13 +0530

    Update branch

--- src/javaproject/Data.java
+++ src/javaproject/Data.java
@@ -1,25 +1,12 @@
 package javaproject;
 
 public class Data {
-<<<<<<< HEAD
 
-	public int sum(int a,int b){
-=======
-	
-public int sum(int a,int b){
->>>>>>> master
-		
-		
-		
-		System.out.println(a+b);
-		return a+b;
-		
-<<<<<<< HEAD
+	public int sum(int a, int b) {
+
+		System.out.println(a + b);
+		return a + b;
+
 	}
-	
-	
-=======
-}
 
->>>>>>> master
 }
