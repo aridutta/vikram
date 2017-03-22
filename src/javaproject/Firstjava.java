33a0b30156d5b4f95ddbf8523a85e4cce8f26d2b
aridutta <arindam.dutta@wildnettechnologies.com>
2017-03-22 13:10:31 +0530

    this is branch commit

--- src/javaproject/Firstjava.java
+++ src/javaproject/Firstjava.java
@@ -6,8 +6,12 @@ public class Firstjava {
 	public void  show(){
 		System.out.println("hello i am show method");
 	}
+	public void  display(){
+		System.out.println("hello i am show method");
+	}
 	public static void main(String[] args) {
 		Firstjava obj=new Firstjava();
 		obj.show();
+		obj.display();
 	}
 }
